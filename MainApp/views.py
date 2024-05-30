b3e1993718c64f0c2d920aaeb7c280f70b78cc5a
Shlok Arora <shlokarora2709@gmail.com>
2024-05-30 18:49:53 +0530

    feat: Add file upload functionality to views.py

---
+++ MainApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import UploadFileForm
+import pandas as pd
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            df=pd.read_excel(file,0)
+            mod_df=df.groupby(['Cust State','DPD']).size().reset_index(name='Count')
+            df_html = mod_df.to_html(index=False)
+            return render(request, 'index.html', {'form': form,'df_html': df_html})
+    else:
+        form = UploadFileForm()
+        return render(request, 'index.html', {'form': form})
